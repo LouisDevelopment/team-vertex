5de792cac69d3de284356d3815a7996a44ea5c92
Louis Braidwood <louisbraidwood@gmail.com>
2024-09-08 10:09:52 +0100

    Added test game

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 import express from 'express';
 const app = express();
 app.use(express.json());
-app.use(express.static('team-vertex/dist'));
+app.use(express.static('team-vertex/dist', {
+    setHeaders: (res, filePath) => {
+        if (filePath.endsWith('.br')) {
+            res.setHeader('Content-Encoding', 'br');
+            res.setHeader('Content-Type', 'application/javascript');
+        } else if (filePath.endsWith('.gz')) {
+            res.setHeader('Content-Encoding', 'gzip');
+            res.setHeader('Content-Type', 'application/javascript');
+        }
+    }
+}));
 const port = process.env.port || 8080;
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
