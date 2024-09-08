2b176832b1e271d104b04f9b0d8d51dca113eb02
Louis Braidwood <louisbraidwood@gmail.com>
2024-09-08 10:15:26 +0100

    fixed wasm.br warning

--- index.js
+++ index.js
@@ -5,9 +5,21 @@ app.use(express.static('team-vertex/dist', {
     setHeaders: (res, filePath) => {
         if (filePath.endsWith('.br')) {
             res.setHeader('Content-Encoding', 'br');
-            res.setHeader('Content-Type', 'application/javascript');
+            if (filePath.endsWith('.wasm.br')) {
+                res.setHeader('Content-Type', 'application/wasm');
+            } else if (filePath.endsWith('.js.br')) {
+                res.setHeader('Content-Type', 'application/javascript');
+            }
         } else if (filePath.endsWith('.gz')) {
             res.setHeader('Content-Encoding', 'gzip');
+            if (filePath.endsWith('.wasm.gz')) {
+                res.setHeader('Content-Type', 'application/wasm');
+            } else if (filePath.endsWith('.js.gz')) {
+                res.setHeader('Content-Type', 'application/javascript');
+            }
+        } else if (filePath.endsWith('.wasm')) {
+            res.setHeader('Content-Type', 'application/wasm');
+        } else if (filePath.endsWith('.js')) {
             res.setHeader('Content-Type', 'application/javascript');
         }
     }
