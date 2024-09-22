c060502d0559ea2fbadf5223eb73d3c042793cc0
Louis Braidwood <louisbraidwood@gmail.com>
2024-09-22 23:36:49 +0100

    Fixed refresh on sub-domains

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 import express from 'express';
 const app = express();
-const path = require('path');
 app.use(express.json());
-app.use(express.static(path.join(__dirname, 'team-vertex/dist'), {
+app.use(express.static('team-vertex/dist', {
     setHeaders: (res, filePath) => {
         if (filePath.endsWith('.br')) {
             res.setHeader('Content-Encoding', 'br');
@@ -25,9 +24,6 @@ app.use(express.static(path.join(__dirname, 'team-vertex/dist'), {
         }
     }
 }));
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'team-vertex/dist', 'index.html'));
-});
 const port = process.env.PORT || 8080;
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
